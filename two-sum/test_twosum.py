6bab08933434e976088c739e8f06c916493f5a8b
John Steinmetz <jsteinme@umich.edu>
2020-06-12 23:20:49 -0400

    added unique and negative tests

--- two-sum/test_twosum.py
+++ two-sum/test_twosum.py
@@ -6,4 +6,18 @@ class TestTwoSum:
         assert(two_sum(nums=[], target=1) == [])
     
     def test_one_plus_one_is_two(self):
-        assert(two_sum(nums=[1, 1], target=1) == [0, 1])
+        assert(two_sum(nums=[1, 1], target=2) == [0, 1])
+
+    def test_returns_empty_for_one_number_list(self):
+        assert(two_sum(nums=[1], target=1) == [])
+
+    def test_for_unique_positive_numbers(self):
+        assert(two_sum(nums=[1, 2, 3, 4, 5], target=9) == [3, 4])
+    
+    def test_for_unique_indices(self):
+        assert(two_sum(nums=[3, 3, 3, 3], target=6) != [0, 0])
+        assert(two_sum(nums=[3, 3, 3, 3], target=6) == [0, 1])
+
+    def test_negative_numbers(self):
+        assert(two_sum(nums=[-1, 2, 4], target=1) == [0, 1])
+        assert(two_sum(nums=[-2, 3, -4], target=-6) == [0, 2])
